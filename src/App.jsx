be235c3bd1cf901249de6ff7d7771d6d2151efd1
Eslashier <anfelov@gmail.com>
2022-04-29 16:57:29 -0500

    added delete button to original exercise

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,9 @@ import ListOfStudents from './components/ListOfStudents'
 
 function App() {
 
-  const [number, setNumber] = useState(0)
-
-  const addToNumber = ()=>{
-    setNumber(number + 1)
-  }
-
-  const substractToNumber = ()=>{
-    setNumber(number - 1)
-  }
 
   return (
     <div>
-      <h1>My first application</h1>
-      <h1>{number}</h1>
-      <button onClick={addToNumber}>+</button>
-      <button onClick={substractToNumber}>-</button>
       <ListOfStudents />
     </div>
   )
--- src/components/ListOfStudents.jsx
+++ src/components/ListOfStudents.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from 'react'
+import './ListOfStudents.css'
 
 const ListOfStudents = () => {
 
   const onAddStudent = (event) => {
     event.preventDefault();
-    if(name && lastName){
+    if (name && lastName) {
       const student = {
         name,
         lastName
       }
       setList([...list, student])
-    } 
+    }
+  }
+
+  const onRemoveStudent = (index, event) => {
+    event.preventDefault(event);
+    if (index > -1){
+    list.splice(index, 1)
+    let auxlist= [...list]
+    setList(auxlist)
+    }
+    
   }
 
   const [list, setList] = useState([])
@@ -18,27 +29,31 @@ const ListOfStudents = () => {
   const [name, setName] = useState('');
   const [lastName, setLastName] = useState('');
 
-  const addName = (event)=>{
+  const addName = (event) => {
     setName(event.target.value)
   }
 
- const addLastName = (event) => {
+  const addLastName = (event) => {
     setLastName(event.target.value)
- }
-
+  }
 
   return (
-    <div>
-      <h1>Hello from the list of students</h1>
+    <div className="center">
+      <h1 className ="centered">Student List</h1>
       <form>
         <label>Name</label>
-        <input onChange={addName} type="text" name="name" />
+        <br></br>
+        <input onChange={addName} type="text" name="name" placeholder="Your name.." />
+        <br></br>
         <label>Last name</label>
-        <input onChange={addLastName}  type="text" name="lastName" />
-        <button onClick={onAddStudent}>add student</button>
+        <br></br>
+        <input onChange={addLastName} type="text" name="lastName" placeholder="Your last name.." />
+        <br></br>
+        <div className="centered"><button onClick={onAddStudent} className="centeredbutton" >add student</button></div>
         {list.map((student, index) => <div key={index}>
           <h2>{student.name}</h2>
           <h2>{student.lastName}</h2>
+          <button id={index} onClick={e => { onRemoveStudent(index, e) }}>Delete</button>
         </div>)}
       </form>
     </div>
